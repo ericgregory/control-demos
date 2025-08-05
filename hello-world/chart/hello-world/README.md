8f4b58c92256d1cad0a8bedb2e73865f7b8030f9
Eric Gregory <eric@cosmonic.com>
2025-08-05 14:45:28 -0400

    update hello-world and add workflow
    Signed-off-by: Eric Gregory <eric@cosmonic.com>

--- hello-world/chart/hello-world/README.md
+++
@@ -1,9 +0,0 @@
-# Cosmonic Control - "Hello World" Helm chart
-
-This is a Helm chart for installing the Cosmonic Control "Hello world" application. The application is comprised of manifests for the following resources:
-
-- [Component](https://cosmonic.com/docs/custom-resources/#component)
-- [Provider](https://cosmonic.com/docs/custom-resources/#provider)
-- [Config](https://cosmonic.com/docs/custom-resources/#config)
-
-You can learn more about these resources at [https://cosmonic.com/docs/custom-resources/](https://cosmonic.com/docs/custom-resources/).
--- hello-world/src/lib.rs
+++ hello-world/src/lib.rs
@@ -8,13 +8,6 @@ impl http::Server for Component {
     fn handle(
         _request: http::IncomingRequest,
     ) -> http::Result<http::Response<impl http::OutgoingBody>> {
-        Ok(http::Response::new("
-        Hello from Cosmonic Control!
-        Your deployment is now running a WebAssembly application with wasmCloud.
-        
-        To learn more about building your own WebAssembly applications, visit:
-        
-        https://wasmcloud.com/docs/tour/hello-world/
-          \n"))
+        Ok(http::Response::new("Hello from Cosmonic Control!\n"))
     }
 }
